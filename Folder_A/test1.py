6dbc4881c6384bc0ef543b7aabd53447063254f1
NagaC365 <munukutla.n.c@gmail.com>
2022-09-16 23:05:00 +0530

    Modified test_A.

---
+++ Folder_A/test1.py
@@ -0,0 +1,10 @@
+# This is a program that computes sum of the two numbers that are integers and are also subset of real numbers and are also natural numbers.
+x = 2
+y = 4
+print(x)
+sum = SuM(a, b)
+print(sum)
+
+# function to return sum of two numbers.
+def SuM(x, y):
+    return x + y
